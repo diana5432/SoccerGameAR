be5a8dd1a342a5366c4b094510bfbcb88ce5477b
Diana Engels <dianapetrich@gmail.com>
2021-05-25 12:59:09 +0200

    Initial commit

---
+++ Assets/Scripts/KickOff.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class KickOff : MonoBehaviour
+{
+    public Vector3 kickDirection;
+    public float maxPower;
+    public float actualPower;
+
+    private Rigidbody rb;
+    private float timeCorrection;
+    private Vector3 startPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            timeCorrection = Time.time;
+        }
+        if (Input.GetButton("Jump"))
+        {
+            actualPower = Mathf.Abs(Mathf.Sin(Time.time - timeCorrection)) * maxPower;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            rb.AddForce(kickDirection * actualPower);
+            rb.AddTorque(Random.insideUnitSphere * actualPower);
+        }
+        // for debugging
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = startPos;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+        }
+    }
+}
---
+++ Assets/Scripts/StatusBarFill.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBarFill : MonoBehaviour
+{
+    public KickOff kickOff;
+    public Image fillImage;
+
+    private Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (slider.value < slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+        if (slider.value > slider.minValue && !fillImage.enabled)
+        {
+            fillImage.enabled = true;
+        }
+       */
+        slider.value = kickOff.actualPower;
+    }
+}
