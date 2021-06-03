3d1feca7115a03d30e0fbee437802e4dd4f5278c
s0570932 <diana.engels@student.htw-berlin.de>
2021-06-03 15:06:19 +0200

    implement first running ar version

--- Assets/Scripts/Controller/GoalController.cs
+++ Assets/Scripts/Controller/GoalController.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class GoalController : Subject
 {
+    [SerializeField] private Transform _baseLineCenter;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
-            Vector3 hitPosition = other.ClosestPoint(transform.position);
-            float hitDistanceFromCenter = Vector3.Distance(hitPosition, transform.position);
+            Vector3 hitPosition = other.ClosestPoint(_baseLineCenter.position);
+            float hitDistanceFromCenter = Vector3.Distance(hitPosition, _baseLineCenter.position);
             Notify(hitDistanceFromCenter, NotificationType.GoalHit);
         }
     }
