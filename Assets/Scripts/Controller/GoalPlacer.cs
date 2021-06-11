b44bc7bd5af3b93d06e0ca0d06380a9f6156d28d
diana5432 <dianapetrich@gmail.com>
2021-06-11 20:18:17 +0200

    add AR raycast goal placer script

--- Assets/Scripts/Controller/GoalPlacer.cs
+++ Assets/Scripts/Controller/GoalPlacer.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class GoalPlacer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GoalPanelController _goalPanel;
+
+    private ARRaycastManager _raycastManager;
+    private List<ARRaycastHit> _hits;
+
+    private void Awake() 
     {
-        
+        _raycastManager = GetComponent<ARRaycastManager>();
+        _hits = new List<ARRaycastHit>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() 
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (_raycastManager.Raycast(touch.position, _hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = _hits[0].pose;
+
+                _goalPanel.SpawnAtPosition(hitPose.position);
+            }
+        }
+
     }
+
+
 }
