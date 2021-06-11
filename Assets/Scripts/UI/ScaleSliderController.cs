695eee8a825963a051eddaffc419079bb90225fe
diana5432 <dianapetrich@gmail.com>
2021-06-11 17:17:27 +0200

    scale goal by slider

---
+++ Assets/Scripts/UI/ScaleSliderController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleSliderController : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    private Slider _slider;
+    private float _currentValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider = GetComponent<Slider>();
+        _currentValue = _slider.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentValue != _slider.value)
+        {
+            _currentValue = _slider.value;
+            _target.localScale = Vector3.one * _currentValue;
+        }
+    }
+}
